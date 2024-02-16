343ea4c3afc0d962b85427dce3ddc3ff8e8262c3
Laurens Lamberts <laurens.lamberts@iodigital.com>
2024-02-16 14:05:08 +0100

    working stereo setup without audio artifacts

--- seed/Surroundings/Surroundings.cpp
+++ seed/Surroundings/Surroundings.cpp
@@ -2,6 +2,7 @@
 #include <string.h>
 #include "daisy_seed.h"
 #include "daisysp.h"
+#include "modules/wavplayer/wavplayer.h"
 
 // Wav files need to be 16 bit - 1 channel (though 2 channels seems fine?) - 48kHz
 
@@ -11,6 +12,8 @@ using namespace daisysp;
 DaisySeed      hardware;
 SdmmcHandler   sdcard;
 FatFSInterface fsi;
+WavPlayer      sampler1Left;
+WavPlayer      sampler1Right;
 WavPlayer      sampler1;
 WavPlayer      sampler2;
 WavPlayer      sampler3;
@@ -22,8 +25,14 @@ WavPlayer      sampler8;
 
 Led led1;
 
-#define SAMPLE_1_ENABLED true
-#define SAMPLE_2_ENABLED true
+// define buffers
+// these are initialized globally on the SDRAM memory sector
+#define BUFSIZE 4096
+int16_t DSY_SDRAM_BSS bufferLeft[BUFSIZE];
+int16_t DSY_SDRAM_BSS bufferRight[BUFSIZE];
+
+#define SAMPLE_1_ENABLED false
+#define SAMPLE_2_ENABLED false
 #define SAMPLE_3_ENABLED false
 #define SAMPLE_4_ENABLED false
 #define SAMPLE_5_ENABLED false
@@ -37,7 +46,10 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
                    AudioHandle::InterleavingOutputBuffer out,
                    size_t                                size)
 {
-    float mixKnobLevel = hardware.adc.GetFloat(0);
+    float samp_out_left  = 0.0f;
+    float samp_out_right = 0.0f;
+    float samp_out       = 0.0f;
+    float mixKnobLevel   = hardware.adc.GetFloat(0);
 
     float volumeKnob1Level = hardware.adc.GetFloat(1);
     float volumeKnob2Level = hardware.adc.GetFloat(2);
@@ -45,14 +57,17 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
     // For now it is mono to both outputs
     for(size_t i = 0; i < size; i += 2)
     {
-        // out[i] = out[i + 1] = 0.0f;
+        // stream samplers
+        samp_out_left += s162f(sampler1Left.Stream());
+        samp_out_right += s162f(sampler1Right.Stream());
+
         if(SAMPLE_1_ENABLED)
         {
             // Storm
             float sampler1Output = s162f(sampler1.Stream());
             sampler1Output *= volumeKnob1Level; // Apply volume level
             sampler1Output *= 1.8f;             // Normalisation
-            out[i] = out[i + 1] = sampler1Output;
+            samp_out += sampler1Output;
         }
         if(SAMPLE_2_ENABLED)
         {
@@ -60,40 +75,41 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
             float sampler2Output = s162f(sampler2.Stream());
             sampler2Output *= volumeKnob2Level; // Apply volume level
             sampler2Output *= 0.6f;             // Normalisation
-            out[i] += out[i + 1] += sampler2Output;
+            samp_out += sampler2Output;
         }
         if(SAMPLE_3_ENABLED)
         {
             float sampler3Output = s162f(sampler3.Stream());
-            out[i] += out[i + 1] += sampler3Output;
+            samp_out += sampler3Output;
         }
         if(SAMPLE_4_ENABLED)
         {
             float sampler4Output = s162f(sampler4.Stream());
-            out[i] += out[i + 1] += sampler4Output;
+            samp_out += sampler4Output;
         }
         if(SAMPLE_5_ENABLED)
         {
             float sampler5Output = s162f(sampler5.Stream());
-            out[i] += out[i + 1] += sampler5Output;
+            samp_out += sampler5Output;
         }
         if(SAMPLE_6_ENABLED)
         {
             float sampler6Output = s162f(sampler6.Stream());
-            out[i] += out[i + 1] += sampler6Output;
+            samp_out += sampler6Output;
         }
         if(SAMPLE_7_ENABLED)
         {
             float sampler7Output = s162f(sampler7.Stream());
-            out[i] += out[i + 1] += sampler7Output;
+            samp_out += sampler7Output;
         }
         if(SAMPLE_8_ENABLED)
         {
             float sampler8Output = s162f(sampler8.Stream());
-            out[i] += out[i + 1] += sampler8Output;
+            samp_out += sampler8Output;
         }
-        out[i] *= mixKnobLevel;
-        out[i + 1] *= mixKnobLevel;
+        // out[i] = out[i + 1] = samp_out * .25f * mixKnobLevel;
+        out[i]     = samp_out_left *= .25f * mixKnobLevel;
+        out[i + 1] = samp_out_right *= .25f * mixKnobLevel;
     }
 
     led1.Set(1.f * volumeKnob1Level);
@@ -102,27 +118,64 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
 
 int main(void)
 {
-    // Configure and Initialize the Daisy Seed
-    // These are separate to allow reconfiguration of any of the internal
-    // components before initialization.
-    // hardware.Configure();
     hardware.Init();
-    hardware.SetAudioBlockSize(4);
+    hardware.SetAudioBlockSize(24);
+    // hardware.StartLog(true);
+
+    // System::Delay(1000);
 
     // hardware.PrintLine("INITIALIZING...");
     // printf("TEST123");
     // fflush(stdout);
 
-    // CONFIGURE SD CARD
+    // Initialize SD card
     SdmmcHandler::Config sd_cfg;
     sd_cfg.Defaults();
     // sd_cfg.speed = daisy::SdmmcHandler::Speed::FAST;
     sd_cfg.width = daisy::SdmmcHandler::BusWidth::BITS_1;
     sdcard.Init(sd_cfg);
     fsi.Init(FatFSInterface::Config::MEDIA_SD);
-    f_mount(&fsi.GetSDFileSystem(), "/", 1);
+
+    hardware.Print("mount...");
+    if(FRESULT result = f_mount(&fsi.GetSDFileSystem(), fsi.GetSDPath(), 0))
+    {
+        hardware.PrintLine("did not mount - err %d", result);
+        asm("bkpt 255");
+    }
+    hardware.PrintLine("ok!");
+    // System::Delay(1000);
+
+    // Initialize first sampler on /waves-left
+    if(int result = sampler1Left.Init("0:/waves-left", bufferLeft, BUFSIZE, 1))
+    {
+        hardware.PrintLine("did not initialize sampler1Left - err %d", result);
+        asm("bkpt 255");
+    }
+    hardware.PrintLine("sampler1Left - loaded %d files",
+                       sampler1Left.GetNumberFiles());
+    // System::Delay(500);
+
+    // Initialize second sampler on /waves-right
+    if(int result
+       = sampler1Right.Init("0:/waves-right", bufferRight, BUFSIZE, 1))
+    {
+        hardware.PrintLine("did not initialize sampler1Right - err %d", result);
+        asm("bkpt 255");
+    }
+    hardware.PrintLine("sampler1Right - loaded %d files",
+                       sampler1Right.GetNumberFiles());
+    // System::Delay(500);
+    // f_mount(&fsi.GetSDFileSystem(), "/", 1);
 
     std::string sdPath = fsi.GetSDPath();
+
+    // Set samplers to looping
+    sampler1Left.SetLooping(true);
+    sampler1Right.SetLooping(true);
+
+    // Open audio files in samplers
+    sampler1Left.Open(0);
+    sampler1Right.Open(0);
 
     // CONFIGURE SAMPLER
     // 1: birds-dutch
@@ -138,7 +191,7 @@ int main(void)
     // 11: stream
     // 12: stream
 
-    if(SAMPLE_1_ENABLED)
+    /* if(SAMPLE_1_ENABLED)
     {
         sampler1.Init(sdPath.c_str());
         sampler1.SetLooping(true);
@@ -185,11 +238,11 @@ int main(void)
         sampler8.Init(sdPath.c_str());
         sampler8.SetLooping(true);
         sampler8.Open(3);
-    }
+    } */
 
-    float samplerate = hardware.AudioSampleRate(); // per second
 
-    // INITIALIZE CONTROLS AND LEDS
+    // Initialize controls and led
+    float            samplerate = hardware.AudioSampleRate(); // per second
     AdcChannelConfig adcConfig
         [NUMBER_OF_ADC_CHANNELS]; // Create an array of AdcChannelConfig
     led1.Init(hardware.GetPin(20), false, samplerate / 48.f); // red LED
@@ -202,12 +255,30 @@ int main(void)
                       NUMBER_OF_ADC_CHANNELS); // Has to be >8 channels later
     hardware.adc.Start();
 
-    // Start the audio callback
+    // Start audio
+    hardware.Print("starting audio...");
     hardware.StartAudio(AudioCallback);
+    hardware.PrintLine("ok!");
 
     // Loop forever
     for(;;)
     {
+        // prepare samples for sampler 1; manage errors
+        if(int result = sampler1Left.Prepare())
+        {
+            hardware.PrintLine("ERROR WHILE PREPARING sampler1Left - err %d",
+                               result);
+            asm("bkpt 255");
+        }
+
+        // prepare samples for sampler 2; manage errors
+        if(int result = sampler1Right.Prepare())
+        {
+            hardware.PrintLine("ERROR WHILE PREPARING sampler1Right - err %d",
+                               result);
+            asm("bkpt 255");
+        }
+
         if(SAMPLE_1_ENABLED)
         {
             sampler1.Prepare();
--- seed/Surroundings/docs/Getting-SD-to-work.md
+++ seed/Surroundings/docs/Getting-SD-to-work.md
@@ -3,23 +3,26 @@ In this tutorial we're going to breadboard a starting point of using an SD card 
 Requirements;
 
 - Daisy seed
-- An SD card, formatted in FAT32 (?). Not sure if there are any max. storage requirements
+- An SD card, formatted in FAT32 with MBR partitioning.
 - STLINK-V3MINIE
 - SD card breakout board (I used this one from Popolu; https://opencircuit.shop/product/breakout-board-for-microsd-card)
 - VS code with Cortex debug extension
 - DaisyExamples repository on your computer
 
 1. Setup the breadboard as shown in diagram (TODO) and connect the debugger to the Daisy Seed
-2. Alter the SDMMC example by adding this line before the line that specifies "sd.Init(sd_cfg);";
+2. Alter the SDMMC example by adding this line before the line that specifies "sd.Init(sd_cfg);
+   `sd_cfg.width = daisy::SdmmcHandler::BusWidth::BITS_1;`
+3. Optional; Alter the SDMMC example by adding this line before the line that specifies "sd.Init(sd_cfg);";
    `sd_cfg.speed = daisy::SdmmcHandler::Speed::SLOW;`
    This will make the SD card run at a slower speed, which required when using breadboard wires. This can later be removed when you start using a PCB with proper traces.
-3. Set a breakpoint at the line that is as follows; "if(f_open(&SDFile, TEST_FILE_NAME, (FA_CREATE_ALWAYS) | (FA_WRITE)) == FR_OK)"
-4. Run SDMMC example in debug mode (using F5 in vscode with cortex debug & STLINK-v3-MINIE)
-5. When the executer is at this line, "step into" and "step over" until you reach "if (res == FR_OK)"
-6. Check the status by hovering over "res";
+4. Set a breakpoint at the line that is as follows; "if(f_open(&SDFile, TEST_FILE_NAME, (FA_CREATE_ALWAYS) | (FA_WRITE)) == FR_OK)"
+5. Run SDMMC example in debug mode (using F5 in vscode with cortex debug & STLINK-v3-MINIE)
+6. When the executer is at this line, "step into" and "step over" until you reach "if (res == FR_OK)"
+7. Check the status by hovering over "res";
    a. FR_DISK_ERR: SD card was not found (check your wiring on the breadboard and ensure the SD card is properly inserted)
    b. FR_NO_FILESYSTEM: SD card was found, but appears not to be formatted correctly. I had this with both FAT and ExFAT, formatted using Mac.
 
 Notes;
 
 - don't be misled by the blinking LED in this example while debugging. It is not always a sign of a success in this case, as the execution won't stop on failure while debugging.
+- If you're connecting an SDCard to play WAV files, I found that WAV files have to be mono, 16 bit an 48khz.
