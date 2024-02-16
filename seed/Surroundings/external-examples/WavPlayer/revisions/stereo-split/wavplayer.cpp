a720d572a070509083fae0d869b0c0265bdde684
Laurens Lamberts <laurens.lamberts@iodigital.com>
2024-02-16 14:04:30 +0100

    added backup original and stereo-split wavplayers

---
+++ seed/Surroundings/external-examples/WavPlayer/revisions/stereo-split/wavplayer.cpp
@@ -0,0 +1,248 @@
+#include <cstring>
+#include "hid/wavplayer.h"
+#include "daisy_seed.h"
+
+using namespace daisy;
+
+FRESULT WavPlayer::Init(const char *search_path,
+                        int16_t    *buffer,
+                        size_t      bufferSize,
+                        size_t      numChannels)
+{
+    buff_        = buffer;
+    bufferSize_  = bufferSize / numChannels;
+    numChannels_ = numChannels;
+
+    // First check for all .wav files, and add them to the list until its full or there are no more.
+    // Only checks '/'
+    FRESULT result = FR_OK;
+    FILINFO fno;
+    DIR     dir;
+    char   *fn;
+    file_sel_ = 0;
+    file_cnt_ = 0;
+    playing_  = false;
+    looping_  = false;
+
+    // Open Dir and scan for files.
+    result = f_opendir(&dir, search_path);
+    if(result != FR_OK)
+    {
+        return result;
+    }
+
+    do
+    {
+        result = f_readdir(&dir, &fno);
+        // Exit if bad read or NULL fname
+        if(result != FR_OK || fno.fname[0] == 0)
+            break;
+        // Skip if its a directory or a hidden file.
+        if(fno.fattrib & (AM_HID | AM_DIR))
+            continue;
+        // Now we'll check if its .wav and add to the list.
+        fn = fno.fname;
+        if(file_cnt_ < kMaxFiles - 1)
+        {
+            if(strstr(fn, ".wav") || strstr(fn, ".WAV"))
+            {
+                strcpy(file_info_[file_cnt_].name, fn);
+                strcpy(file_info_[file_cnt_].path, search_path);
+
+                // add an extra slash for non-root directories
+                if(strcmp(search_path, "0:/") != 0)
+                {
+                    strcat(file_info_[file_cnt_].path, "/");
+                }
+
+                strcat(file_info_[file_cnt_].path, fn);
+                file_cnt_++;
+                // For now lets break anyway to test.
+                //                break;
+            }
+        }
+        else
+        {
+            break;
+        }
+    } while(result == FR_OK);
+    f_closedir(&dir);
+    // Now we'll go through each file and load the WavInfo.
+    for(size_t i = 0; i < file_cnt_; i++)
+    {
+        size_t bytesread;
+        if(f_open(&fil_, file_info_[i].path, (FA_OPEN_EXISTING | FA_READ))
+           == FR_OK)
+        {
+            // Populate the WAV Info
+            result = f_read(&fil_,
+                            (void *)&file_info_[i].raw_data,
+                            sizeof(WAV_FormatTypeDef),
+                            &bytesread);
+            if(result != FR_OK)
+            {
+                // Maybe add return type
+                return result;
+            }
+
+            // add length
+            file_info_[i].length = file_info_[i].raw_data.FileSize
+                                   - file_info_[i].raw_data.SubChunk1Size
+                                   - sizeof(WAV_FormatTypeDef);
+
+            file_info_[i].length /= 2; // 16-bit
+
+            f_close(&fil_);
+        }
+    }
+
+    // initialize at Nothing
+    buff_state_ = BUFFER_STATE_IDLE;
+    read_ptr_ = read_ptr_abs_ = 0;
+
+    return result;
+}
+
+
+int WavPlayer::Open(size_t sel)
+{
+    if(sel != file_sel_)
+    {
+        f_close(&fil_);
+        file_sel_ = sel < file_cnt_ ? sel : file_cnt_ - 1;
+    }
+
+    // Set Buffer Position
+    if(int result = f_open(
+           &fil_, file_info_[file_sel_].path, (FA_OPEN_EXISTING | FA_READ)))
+    {
+        return result;
+    }
+    else
+    {
+        // prepare buffer for reading...
+        read_ptr_ = read_ptr_abs_ = 0;
+        buff_state_               = BUFFER_STATE_PREPARE_0;
+        Restart();
+        Prepare();
+
+        return result;
+    }
+}
+
+int WavPlayer::OpenById(const char *id)
+{
+    // add extension
+    // find index based on filename
+    int idx = -1;
+    for(size_t i = 0; i < kMaxFiles; i++)
+    {
+        if(strstr(file_info_[i].name, id) != nullptr)
+        {
+            idx = i;
+            break;
+        }
+    }
+
+    if(idx >= 0)
+        return Open(idx);
+    else
+        return FR_INVALID_NAME;
+}
+
+int WavPlayer::Close()
+{
+    return f_close(&fil_);
+}
+
+int16_t WavPlayer::Stream()
+{
+    int16_t samp;
+    if(playing_)
+    {
+        samp = buff_[read_ptr_];
+        // Increment rpo
+        read_ptr_ = (read_ptr_ + 1) % (bufferSize_ * numChannels_);
+        if(read_ptr_ == 0)
+            buff_state_ = BUFFER_STATE_PREPARE_1;
+        else if(read_ptr_ == bufferSize_ * numChannels_ / 2)
+            buff_state_ = BUFFER_STATE_PREPARE_0;
+
+        // absolute pointer
+        read_ptr_abs_++;
+        read_ptr_abs_ %= file_info_[file_sel_].length;
+    }
+    else
+    {
+        samp = 0;
+        if(looping_)
+            playing_ = true;
+    }
+    return samp;
+}
+
+int WavPlayer::Prepare()
+{
+    FRESULT readres = FR_OK;
+
+    if(buff_state_ != BUFFER_STATE_IDLE)
+    {
+        size_t offset, bytesread, rxsize;
+        bytesread = 0;
+        rxsize    = bufferSize_ * numChannels_ * sizeof(buff_[0]) / 2;
+        offset    = buff_state_ == BUFFER_STATE_PREPARE_1
+                        ? bufferSize_ * numChannels_ / 2
+                        : 0;
+
+        readres = f_read(&fil_, &buff_[offset], rxsize, &bytesread);
+
+        if(bytesread < rxsize || f_eof(&fil_))
+        {
+            if(looping_)
+            {
+                Restart();
+                readres = f_read(&fil_,
+                                 &buff_[offset + (bytesread / 2)],
+                                 rxsize - bytesread,
+                                 &bytesread);
+            }
+            else
+            {
+                playing_ = false;
+            }
+        }
+        buff_state_ = BUFFER_STATE_IDLE;
+    }
+
+    return readres;
+}
+
+void WavPlayer::Restart()
+{
+    f_lseek(&fil_,
+            sizeof(WAV_FormatTypeDef)
+                + file_info_[file_sel_].raw_data.SubChunk1Size);
+}
+
+WavPlayer::BufferState WavPlayer::GetNextBuffState()
+{
+    size_t next_samp;
+    next_samp = (read_ptr_ + 1) % bufferSize_;
+    if(next_samp < bufferSize_ / 2)
+    {
+        return BUFFER_STATE_PREPARE_1;
+    }
+    else
+    {
+        return BUFFER_STATE_PREPARE_0;
+    }
+}
+
+uint32_t WavPlayer::TimeUntilEOF()
+{
+    if(!playing_)
+        return 0;
+    return (float)((file_info_[file_sel_].length - read_ptr_abs_) / numChannels_
+                   / 48000.0)
+           * 1000;
+};
---
+++ seed/Surroundings/external-examples/WavPlayer/revisions/stereo-split/wavplayer.h
@@ -0,0 +1,127 @@
+/* Current Limitations:
+- 1x Playback speed only
+- 16-bit, mono files only (otherwise fun weirdness can happen).
+- Only 1 file playing back at a time.
+- Not sure how this would interfere with trying to use the SDCard/FatFs outside of
+this module. However, by using the extern'd SDFile, etc. I think that would break things.
+*/
+#pragma once
+#ifndef DSY_WAVPLAYER_H
+#define DSY_WAVPLAYER_H /**< Macro */
+#include "daisy_core.h"
+#include "util/wav_format.h"
+#include "ff.h"
+
+#define WAV_FILENAME_MAX \
+    256 /**< Maximum LFN (set to same in FatFs (ffconf.h) */
+
+namespace daisy
+{
+// TODO: add bitrate, samplerate, length, etc.
+/** Struct containing details of Wav File. */
+struct WavFileInfo
+{
+    WAV_FormatTypeDef raw_data;               /**< Raw wav data */
+    char              name[WAV_FILENAME_MAX]; /**< Wav filename */
+    char              path[WAV_FILENAME_MAX]; /**< Wav filepath */
+    uint32_t          length;                 /**< in samples   */
+};
+
+/* 
+TODO:
+- Make template-y to reduce memory usage.
+*/
+
+
+/** Wav Player that will load .wav files from an SD Card,
+and then provide a method of accessing the samples with
+double-buffering. */
+class WavPlayer
+{
+  public:
+    WavPlayer() {}
+    ~WavPlayer() {}
+
+    /** Initializes the WavPlayer, loading up to max_files of wav files from an SD Card. */
+    FRESULT Init(const char* search_path,
+                 int16_t*    buffer,
+                 size_t      bufferSize,
+                 size_t      numChannels = 1);
+
+    /** Opens the file at index sel for reading.
+    \param sel File to open
+     */
+    int Open(size_t sel);
+
+    /** Opens the file by name for reading.
+    \param id ID of file to open
+     */
+    int OpenById(const char* id);
+
+    /** Closes whatever file is currently open.
+    \return &
+     */
+    int Close();
+
+    /** \return The next sample if playing, otherwise returns 0 */
+    int16_t Stream();
+
+    /** Collects buffer for playback when needed. */
+    int Prepare();
+
+
+    inline void Play() { playing_ = true; };
+    inline void Pause() { playing_ = false; };
+
+    /** Resets the playback position to the beginning of the file immediately */
+    void Restart();
+
+    /** Sets whether or not the current file will repeat after completing playback. 
+    \param loop To loop or not to loop.
+    */
+    inline void SetLooping(bool loop) { looping_ = loop; }
+
+    /** \return Whether the WavPlayer is looping or not. */
+    inline bool GetLooping() const { return looping_; }
+
+    /** \return The number of files loaded by the WavPlayer */
+    inline size_t GetNumberFiles() const { return file_cnt_; }
+
+    /** \return currently selected file.*/
+    inline size_t GetCurrentFile() const { return file_sel_; }
+
+    inline const char* GetNameOf(const size_t idx_) const
+    {
+        return file_info_[idx_].name;
+    }
+
+    /** \return time left until the end of the playing file */
+    uint32_t TimeUntilEOF();
+
+  private:
+    enum BufferState
+    {
+        BUFFER_STATE_IDLE,
+        BUFFER_STATE_PREPARE_0,
+        BUFFER_STATE_PREPARE_1,
+    };
+
+    BufferState GetNextBuffState();
+
+    static constexpr size_t kMaxFiles = 26;
+
+    int16_t*    buff_;
+    size_t      bufferSize_;
+    size_t      numChannels_;
+    WavFileInfo file_info_[kMaxFiles];
+    size_t      file_cnt_, file_sel_;
+    BufferState buff_state_;
+    size_t      read_ptr_;
+    uint32_t    read_ptr_abs_;
+    bool        looping_, playing_;
+    FIL         fil_;
+};
+
+} // namespace daisy
+
+#endif
